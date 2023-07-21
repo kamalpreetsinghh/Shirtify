e3ffbb19420f70d665d1af532ccf77bf2c4b8541
Kamal Preet Singh <kamalpreetsingh025@gmail.com>
2023-07-21 11:58:08 -0400

    React Environment Variables for website url

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -60,15 +60,18 @@ const Customizer = () => {
     try {
       setGeneratingImg(true);
 
-      const response = await fetch("http://localhost:8080/api/v1/dalle", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          prompt,
-        }),
-      });
+      const response = await fetch(
+        `${import.meta.env.VITE_WEBSITE_URL}/api/v1/dalle`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            prompt,
+          }),
+        }
+      );
 
       const data = await response.json();
 
