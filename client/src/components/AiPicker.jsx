300dc700403ac02262cc56ebe3de38e18b1fd2b6
Kamal Preet Singh <kamalpreetsingh025@gmail.com>
2023-07-21 10:45:33 -0400

    Server to send Image from chat gpt api

--- client/src/components/AiPicker.jsx
+++ client/src/components/AiPicker.jsx
@@ -1,7 +1,40 @@
 import React from "react";
 
-const AiPicker = () => {
-  return <div>AiPicker</div>;
+import Button from "./Button";
+
+const AIPicker = ({ prompt, setPrompt, generatingImg, handleSubmit }) => {
+  return (
+    <div className="aipicker-container">
+      <textarea
+        placeholder="Ask AI..."
+        rows={5}
+        value={prompt}
+        onChange={(e) => setPrompt(e.target.value)}
+        className="aipicker-textarea"
+      />
+      <div className="flex flex-wrap gap-3">
+        {generatingImg ? (
+          <Button type="outline" title="Asking AI..." customStyles="text-xs" />
+        ) : (
+          <>
+            <Button
+              type="outline"
+              title="AI Logo"
+              handleClick={() => handleSubmit("logo")}
+              customStyles="text-xs"
+            />
+
+            <Button
+              type="filled"
+              title="AI Full"
+              handleClick={() => handleSubmit("full")}
+              customStyles="text-xs"
+            />
+          </>
+        )}
+      </div>
+    </div>
+  );
 };
 
-export default AiPicker;
+export default AIPicker;
